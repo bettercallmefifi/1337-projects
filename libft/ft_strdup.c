8d96b3de6c5469efb15b09c1c9315a664458252e
bettercallmefifi <ferdaouskai@gmail.com>
2025-09-24 21:45:13 +0100

    done

---
+++ libft/ft_strdup.c
@@ -0,0 +1,25 @@
+#include <libft.h>
+// #include <stdlib.h>
+// #include <stdio.h>
+
+char	*ft_strdup(const char *s)
+{
+	char	*str;
+	size_t	i;
+	size_t	len;
+
+	i = 0;
+	len = 0;
+	while (s[len])
+		len++;
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	while (s[i])
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
